f881a618b7874dffde4deb113c4ac6784706eadd
Nils Thorin <thorin.nils@gmail.com>
2023-02-16 23:56:05 +0100

    working first model

--- model1.py
+++ model1.py
@@ -1,51 +1,94 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import math
-import sys
+import numpy as np
 
 class Car:
     def __init__(self, v_max, reaction_time, acceleration, position) -> None:
-        self.v_max = v_max
-        self.r = reaction_time
-        self.a = acceleration
-        self.pos = position
-        self.v = v_max
+        self.v_max : int = v_max
+        self.r : int = reaction_time
+        self.a : int = acceleration
+        self.pos : int = position
+        self.v : int = v_max
+        self.pre_car : Car = None
+
+    def __repr__(self) -> str:
+        return "Car(v=%i, r=%i, a=%i, pos=%i, v_max=%i)" % (self.v, self.r, self.a, self.pos, self.v_max)
+
+class MemCell:
+    def __init__(self, v, pos) -> None:
+        self.v = v
+        self.pos = pos
+
+    def __repr__(self) -> str:
+        return "MemCell(v=%i, pos=%i)" % (self.v, self.pos)
 
 N = 50
-V_MAX = 60
-D_TOT = 314
+V_MAX = 400
+D_TOT = 31415
 REACTION_TIME = 1
-ACCELERATION = 1
+ACCELERATION = 30
+dt = 1
 
 def setup_cars():
-    cars = [Car(V_MAX, REACTION_TIME, ACCELERATION, i/N*D_TOT) for i in range(N)]
-    return cars
+    cars = [Car(V_MAX, REACTION_TIME, ACCELERATION, int(i/N*D_TOT)) for i in range(N)]
+    memory = []
 
-def draw(cars):
+    for i in range(REACTION_TIME):
+        time_instance = []
+        for car in cars:
+            time_instance.append(MemCell(car.v, car.pos))
+        memory.append(time_instance)
+
+    cars[0].v = 0
+    cars[1].v = 0
+    cars[2].v = 0
+    return cars, memory
+
+def move(car, pre_speed, dt):
+    if car.v > pre_speed:
+        car.v -= car.a * dt
+    elif car.v >= car.v_max:
+        car.v = car.v_max
+    else:
+        car.v += car.a * dt
+    car.pos = (car.pos + car.v * dt) % D_TOT
+
+def update_mem(mem, speeds):
+    mem.pop()
+    mem.insert(0, speeds)
+    return mem
+
+def update(cars, memory):
+    new_mem = []
+    for i, car in enumerate(cars):
+        new_mem.append(MemCell(car.v, car.pos))
+        move(car, memory[car.r - 1][(i + 1) % N].v, dt)
+    memory = update_mem(memory, new_mem)
+
+def draw(cars, memory):
     fig = plt.figure()
     axis = plt.axes(xlim=(-1.1, 1.1), ylim=(-1.1, 1.1))
-    dt = 0.1
+    axis.set_aspect('equal')
+    line, = axis.plot([], [], "rs")
 
-    def animate(i):
-        axis.clear()
-        axis.set_aspect(1)
-        drawing_uncolored_circle = plt.Circle((0, 0), 1 , fill = False)
-        axis.add_artist(drawing_uncolored_circle)
-        for car in cars:
-            theta = (2*math.pi)/D_TOT * car.pos
-            car.pos += dt*car.v
-            plt.plot(math.cos(theta), math.sin(theta), "x")
+    def init():
+        thetas = [2*np.pi * car.pos/D_TOT for car in cars]
+        line.set_data(np.cos(thetas), np.sin(thetas))
+        circle_road = plt.Circle((0, 0), 1 , fill = False)
+        axis.add_artist(circle_road)
+        return line,
 
-    anim = FuncAnimation(fig, animate, interval=20, repeat=False)
-    # anim.save('cars.mp4', writer='ffmpeg', fps=30)
-    # plt.title('Circle')
-    # plt.show()
+    def animate(frame):
+        update(cars, memory)
+        thetas = [2*np.pi * memcell.pos/D_TOT for memcell in memory[0]]
+        line.set_data(np.cos(thetas), np.sin(thetas))
+        return line,
+
+    anim = FuncAnimation(fig, animate, init_func=init, interval=40, blit=True)
     plt.show()
-    print("AFTER SHOW")
-    sys.exit()
 
 def main():
-    cars = setup_cars()
-    draw(cars)
+    cars, memory = setup_cars()
+    draw(cars, memory)
 
 main()
