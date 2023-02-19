d45338ab1a9112eefbdfa9c4a69da08ae83011bd
Nils Thorin <thorin.nils@gmail.com>
2023-02-19 16:28:54 +0100

    added correct crash checking

--- model2.py
+++ model2.py
@@ -3,19 +3,21 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 class Car:
-    def __init__(self, v_max : int, reaction_time : int, acceleration : int , position : int) -> None:
+    def __init__(self, id : int, v_max : int, reaction_time : int, acceleration : int, retardation : int, position : int) -> None:
+        self.id = id
         self.v_max  = v_max
         self.r = reaction_time
         self.a = acceleration
+        self.ret = retardation
         self.pos = position
         self.v = v_max
         self.pre_car = None
 
     def __repr__(self) -> str:
-        return "Car(v=%i, r=%i, a=%i, pos=%i, v_max=%i)" % (self.v, self.r, self.a, self.pos, self.v_max)
+        return "Car(id=%i, v=%i, r=%i, a=%i, pos=%i, v_max=%i)" % (self.id, self.v, self.r, self.a, self.pos, self.v_max)
 
     def d_max(self) -> int:
-        return int(self.v**2 / (2*self.a) + self.r * self.v)
+        return int(-self.v**2 / (2*self.ret) + self.r * self.v)
 
 class MemCell:
     def __init__(self, v : int, pos : int) -> None:
@@ -27,15 +29,16 @@ class MemCell:
 
 Memory = list[list[MemCell]]
 
-N = 50
-V_MAX = 400
+N = 30
+V_MAX = 3000
 D_TOT = 31415
-REACTION_TIME = 1
+REACTION_TIME = 2
 ACCELERATION = 30
-dt = 1
+RETARDATION = -20
+DT = 1
 
 def setup_cars() -> tuple[list[Car], Memory]:
-    cars = [Car(V_MAX, REACTION_TIME, ACCELERATION, int(i/N*D_TOT)) for i in range(N)]
+    cars = [Car(i, V_MAX, REACTION_TIME, ACCELERATION, RETARDATION, int(i/N*D_TOT)) for i in range(N)]
     memory = []
 
     for i in range(REACTION_TIME):
@@ -44,9 +47,10 @@ def setup_cars() -> tuple[list[Car], Memory]:
             time_instance.append(MemCell(car.v, car.pos))
         memory.append(time_instance)
 
-    cars[0].v = 0
+    # cars[0].v = int(cars[0].v / 5)
     cars[1].v = 0
-    cars[2].v = 0
+    # cars[2].v = 0
+    print(cars[1].pos - cars[0].pos)
     return cars, memory
 
 def update_mem(mem : Memory, new_mem_row : list[MemCell]) -> Memory:
@@ -54,14 +58,15 @@ def update_mem(mem : Memory, new_mem_row : list[MemCell]) -> Memory:
     mem.insert(0, new_mem_row)
     return mem
 
-def move(car : Car, pre_mem : MemCell, dt : int) -> None:
-    if -1000 < (pre_mem.pos - car.pos) < 0:
+def move(car : Car, pre_mem : MemCell, pre_car : Car, dt : int) -> None:
+    # print(car.v)
+    if -10000 < (pre_car.pos - car.pos) < 0:
         print("CRASH!!!")
-        print(car.d_max())
+        print(car)
     if car.d_max() > ((pre_mem.pos - car.pos) % D_TOT) and car.v < car.v_max: # if car in front is far enough away, and we aren't speeding -> accelerate
         car.v += car.a * dt
     elif car.v > pre_mem.v or car.v >= car.v_max: # if we are close and we are faster than car in front, or we are moving over our max speed -> slow down
-        car.v -= car.a * dt
+        car.v = max(car.v + car.ret * dt, 0)
     else: # if we are moving slower than the car in front -> accelerate
         car.v += car.a * dt
     car.pos = (car.pos + car.v * dt) % D_TOT
@@ -70,7 +75,8 @@ def update(cars : list[Car], memory : Memory) -> None:
     new_mem = []
     for i, car in enumerate(cars):
         new_mem.append(MemCell(car.v, car.pos))
-        move(car, memory[car.r - 1][(i + 1) % N], dt)
+        move(car, memory[car.r - 1][(i + 1) % N], cars[(i + 1) % N], DT)
+    # print("NEW TIME STEP")
     memory = update_mem(memory, new_mem)
 
 def draw(cars : list[Car], memory : Memory) -> None:
